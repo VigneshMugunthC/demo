f17288acbe6181121bf86c650bd2635b162d8f46
VigneshMugunthC <vigmug2@gmail.com>
2022-07-04 13:14:21 +0530

    Modified abc.java

--- abc.java
+++ abc.java
@@ -1,6 +1,7 @@
 public class abc{
 	public static void main(String[] args)
 	{
-		System.out.println("Hi Everybody");
+		System.out.println("Hello Everybody");
+		System.out.println("How are you");
 	}
 }
